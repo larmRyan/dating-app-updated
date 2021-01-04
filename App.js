ac8d25c82cf5810408596cfc2b715284a9442562
larmRyan <42777659+larmRyan@users.noreply.github.com>
2021-01-04 12:21:44 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,106 @@
+import React from "react";
+import "./styles.css";
+import Footer from "./Footer";
+import Signup from "./Onboarding/Signup";
+import Login from "./Onboarding/Login";
+import Home from "./Home";
+import PrivateRoute from "./PrivateRoute";
+import { AuthProvider } from "./contexts/AuthContext";
+import Log from "./Onboarding/Log";
+import Logout from "./Logout";
+import CreateAlgo from "./Onboarding/CreateAlgo";
+import Welcome from "./Onboarding/Welcome";
+import LoginControl from "./Onboarding/LoginControl";
+import BioInfo from "./Onboarding/BioInfo";
+import Chats from "./Chats";
+import Header from "./Header";
+import Chatscreen from "./Chatscreen";
+import TwoTruths from "./twoTOneL";
+import Profile from "./Profile";
+import Questions from "./Questions";
+import TwoTruthsOneLie from "./TwoTruthsOneLie";
+import Home2 from "./Home2";
+import Chats2 from "./Chats2";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+
+            <Route path="/createalgo">
+              <CreateAlgo />
+            </Route>
+
+            <Route path="/bioinfo">
+              <BioInfo />
+            </Route>
+
+            <Route path="/login">
+              <Login />
+            </Route>
+
+            <Route path="/logincontrol">
+              <LoginControl />
+            </Route>
+
+            <Route path="/welcome">
+              <Welcome />
+            </Route>
+
+            <Route path="/chat/:person">
+              <Header backButton="/chat2" />
+              <Chatscreen />
+            </Route>
+
+            <Route path="/chat">
+              <h1 className="title"> CHAT </h1>
+              <Chats />
+            </Route>
+
+            <Route path="/profile">
+              <h1 className="title"> PROFILE </h1>
+              <Footer />
+              <Profile />
+            </Route>
+
+            <Route path="/2T1L">
+              <Header backButton="/home" />
+              <h1 className="title"> 2 truths 1 lie </h1>
+              <TwoTruths />
+            </Route>
+
+            {/* Footer shows here */}
+            <Route path="/questions">
+              <Questions />
+            </Route>
+
+            <Route path="/ttol">
+              <TwoTruthsOneLie />
+            </Route>
+
+            <Route path="/chat2">
+              <Chats2 />
+              <Footer home="home2" />
+            </Route>
+
+            <Route path="/home2">
+              <Home2 />
+              <Footer chat="chat2" />
+            </Route>
+
+            <PrivateRoute path="/home">
+              <Home />
+            </PrivateRoute>
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </div>
+  );
+}
