317b65da4b8abb67e66a49377f766b3c9f1b0ab1
larmRyan <42777659+larmRyan@users.noreply.github.com>
2021-01-04 12:21:29 -0600

    Create Anonppl2.js

---
+++ Anonppl2.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Anonppl.css";
+import { Link } from "react-router-dom";
+
+export default function Anonppl() {
+  return (
+    <div class="cards">
+      {/* <Link to="/2t1l">
+         <button className="anonButton"> ANON PANDA </button> 
+      </Link> */}
+      <button className="anonButton"> ANON MOUSE </button>
+    </div>
+  );
+}
