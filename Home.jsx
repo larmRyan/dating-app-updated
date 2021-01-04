f892e1c9a0e252791b80ff83bea9690139182ea8
larmRyan <42777659+larmRyan@users.noreply.github.com>
2021-01-04 12:23:04 -0600

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Button, Alert } from "react-bootstrap";
+import MasqToggle from "./MasqToggle";
+import Anonppl from "./Anonppl";
+import { useAuth } from "./contexts/AuthContext";
+import "./styles.css";
+import { Link, useHistory } from "react-router-dom";
+
+export default function Home() {
+  const [error, setError] = useState("");
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
+
+  async function handleLogout() {
+    setError("");
+
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to logout");
+    }
+  }
+
+  return (
+    <div>
+      {error && <Alert variant="danger">{error}</Alert>}
+      <h1 className="title"> MASQUED MATCHES </h1>
+      <MasqToggle />
+      {/* <Anonppl /> */}
+      {/* <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div> */}
+    </div>
+  );
+}
