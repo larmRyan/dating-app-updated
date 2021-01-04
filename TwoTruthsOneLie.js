4e255c91eeab08f3e65b509c0bb3ccdfd86f30fb
larmRyan <42777659+larmRyan@users.noreply.github.com>
2021-01-03 23:03:02 -0600

    Update TwoTruthsOneLie.js

--- TwoTruthsOneLie.js
+++ TwoTruthsOneLie.js
@@ -2,40 +2,36 @@ import React, { useState } from "react";
 import "./TwoTruthsOneLie.css";
 import { useAuth } from "./contexts/AuthContext";
 import { db } from "./Firebase";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
+// remove the link to questions
 export default function TwoTruthsOneLie(props) {
   const [truth1, setTruth1] = useState("");
   const [truth2, setTruth2] = useState("");
   const [lie, setLie] = useState("");
   const { currentUser } = useAuth();
+  const history = useHistory();
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-
     setTruth1(event.target.truth1.value);
     setTruth2(event.target.truth2.value);
     setLie(event.target.lie.value);
 
-    if (truth1 !== "" && truth2 !== "" && lie !== "") {
-      try {
-        db.collection("users")
-          .doc(currentUser.uid)
-          .update({
-            ttol: {
-              truth1: truth1,
-              truth2: truth2,
-              lie: lie
-            }
-          });
-
-        return <redirect from="/ttol" to="questions" />;
-      } catch (error) {
-        console.error(error.name);
-        console.error(error.message);
-      }
-    } else {
-      console.log("Some/all of the forms weren't filled out");
+    try {
+      db.collection("users")
+        .doc(currentUser.email)
+        .update({
+          ttol: {
+            truth1: truth1,
+            truth2: truth2,
+            lie: lie
+          }
+        });
+      history.push("questions");
+    } catch (error) {
+      console.error(error.name);
+      console.error(error.message);
     }
   };
 
@@ -44,22 +40,24 @@ export default function TwoTruthsOneLie(props) {
       <form className="TwoTruthsOneLie" onSubmit={handleSubmit}>
         <ul>
           <li className="form-row truth">
-            <input id="truth1" type="text" name="truth1" />
+            <input id="truth1" type="text" name="truth1" required />
             <label htmlFor="truth1">First Truth</label>
           </li>
 
           <li className="form-row truth">
-            <input id="truth2" type="text" name="truth2" />
+            <input id="truth2" type="text" name="truth2" required />
             <label htmlFor="truth2">Second Truth</label>
           </li>
 
           <li className="form-row lie">
             <label htmlFor="lie">Lie</label>
-            <input id="lie" type="text" name="lie" />
+            <input id="lie" type="text" name="lie" required />
           </li>
 
           <li className="form-row">
+            {/* <Link to="/questions"> */}
             <input type="submit" value="Send" />
+            {/* </Link> */}
           </li>
         </ul>
       </form>
