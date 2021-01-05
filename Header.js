5a99159d2976c3e5f330e088fe77220f0bb6553c
annieyzhao <42821820+annieyzhao@users.noreply.github.com>
2021-01-04 18:44:33 -0600

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,20 +1,44 @@
 import React from "react";
 import "./Header.css";
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import ArrowLeftOutlinedIcon from "@material-ui/icons/ArrowLeftOutlined";
 import IconButton from "@material-ui/core/IconButton";
 import { Link, useHistory } from "react-router-dom";
 
-export default function Header({ backButton }) {
-  const history = useHistory();
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 500,
+  },
+});
+
+export default function SimpleBottomNavigation({ backButton }) {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className="header"
+    >
+      <Link to={`/${backButton}`}>
+        <BottomNavigationAction className="header__icon" label="" icon={<ArrowLeftOutlinedIcon />} />
+      </Link>
+    </BottomNavigation>
+  );
+}
+
+function Header({ backButton }) {
   return (
     <div className="header">
-      {backButton ? (
-        <IconButton onClick={() => history.replace(backButton)}>
+        <IconButton>
           <ArrowLeftOutlinedIcon className="header__icon" fontSize="medium" />
         </IconButton>
-      ) : (
-        <h1> h </h1>
-      )}
     </div>
   );
 }
